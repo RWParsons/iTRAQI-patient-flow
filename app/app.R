97a01f4dcfa603291b86e9358b385ceb80b6ade6
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2023-05-22 14:33:27 +1000

    colour polylines by transport mode

--- app/app.R
+++ app/app.R
@@ -6,7 +6,7 @@ iTRAQI_vis_app <- function(iTRAQI_paths, facilities) {
   iTRAQI_paths <- process_iTRAQI_paths(iTRAQI_paths)
   facilities <- process_facilities(facilities)
   polyline_paths <- process_polyline_paths(iTRAQI_paths, facilities)
-  
+  # return(polyline_paths)
   ui <- 
     navbarPage(
       "iTRAQI",
@@ -27,39 +27,32 @@ iTRAQI_vis_app <- function(iTRAQI_paths, facilities) {
   
   server <- function(input, output, session) {
     output$map <- renderLeaflet({
-      base_map(map_bounds, facilities, iTRAQI_paths)
+      base_map(
+        map_bounds = map_bounds, 
+        facilities = facilities, 
+        iTRAQI_paths = iTRAQI_paths,
+        polyline_paths = polyline_paths
+      )
     })
     
     observeEvent(input$map_marker_click, {
-      print(input$map_marker_click)
-      df_destinations <-
-        iTRAQI_paths |> 
-        filter(town_point == input$map_marker_click$id) |> 
-        select(town_point, destination1, destination2) |>
-        pivot_longer(!town_point, names_to = "destination_order", values_to = "destination") |> 
-        mutate(destination = toupper(destination)) |> 
-        left_join(
-          select(facilities, FACILITY_NAME_Clean, FCLTY_ID, xcoord, ycoord), 
-          by = c("destination" = "FACILITY_NAME_Clean")
-        )
       
-      df_destinations_plus_origin <- bind_rows(
-        select(filter(iTRAQI_paths, town_point == input$map_marker_click$id), xcoord, ycoord),
-        df_destinations
-      )
+      selected_town_point <- input$map_marker_click$id
+      # print(input$map_marker_click)
       
-      # df_paths_i <- df_paths |> filter(id == paste0("P", input$map_marker_click$id))
+      polyline_selected <-
+        polyline_paths |> 
+        filter(town_point == selected_town_point)
       
-      hide_fcltys <- facilities$FCLTY_ID[!facilities$FCLTY_ID %in% df_destinations$FCLTY_ID]
+      hide_fcltys <- facilities$FCLTY_ID[!facilities$FCLTY_ID %in% polyline_selected$FCLTY_ID]
+      hide_town_points <- iTRAQI_paths$town_point[iTRAQI_paths$town_point != selected_town_point]
       
       leafletProxy("map") |> 
-        hideGroup(paste0("F", hide_fcltys)) |> 
-        showGroup(paste0("F", df_destinations$FCLTY_ID)) |> 
-        addPolylines(
-          lng = df_destinations_plus_origin$xcoord,
-          lat = df_destinations_plus_origin$ycoord
-        )
-      browser()
+        hideGroup(paste0("F", hide_fcltys)) |>
+        showGroup(paste0("F", polyline_selected$FCLTY_ID)) |>
+        hideGroup(paste0("PL", hide_town_points)) |>
+        showGroup(paste0("PL", selected_town_point))
+        
     })
     
   }
--- app/base-map.R
+++ app/base-map.R
@@ -1,7 +1,15 @@
 # base-map.R
 
-base_map <- function(map_bounds, facilities, iTRAQI_paths) {
-  leaflet(options = leafletOptions(minZoom = 5)) |> 
+base_map <- function(map_bounds, facilities, iTRAQI_paths, polyline_paths) {
+  if("base-map.rds" %in% list.files(fixtures_path)) {
+    return(readRDS(file.path(fixtures_path, "base-map.rds")))
+  }
+  
+  sample_town_points <- unique(iTRAQI_paths$town_point)#[1:50]
+  
+  iTRAQI_paths <- filter(iTRAQI_paths, town_point %in% sample_town_points)
+  
+  map <- leaflet(options = leafletOptions(minZoom = 5)) |> 
     setMaxBounds(
       lng1 = map_bounds$lng1, lat1 = map_bounds$lat1,
       lng2 = map_bounds$lng2, lat2 = map_bounds$lat2
@@ -15,6 +23,7 @@ base_map <- function(map_bounds, facilities, iTRAQI_paths) {
                      group = "map labels"
     ) |> 
     hideGroup(paste0("F", facilities$FCLTY_ID)) |> 
+    hideGroup(paste0("PL", polyline_paths$town_point)) |> 
     addCircleMarkers(
       layerId = iTRAQI_paths$town_point,
       lng = iTRAQI_paths$xcoord, 
@@ -24,7 +33,6 @@ base_map <- function(map_bounds, facilities, iTRAQI_paths) {
       popup = iTRAQI_paths$popup
     ) |> 
     addCircleMarkers(
-      layerId = paste0("F", facilities$FCLTY_ID),
       group = paste0("F", facilities$FCLTY_ID),
       lng = facilities$xcoord,
       lat = facilities$ycoord,
@@ -32,4 +40,29 @@ base_map <- function(map_bounds, facilities, iTRAQI_paths) {
       fillOpacity = 0.2,
       color = "red"
     )
+  
+  pb <- progress_bar$new(total = length(sample_town_points))
+  
+  for(tp in sample_town_points) {
+
+    polyline_select <- polyline_paths |>
+      filter(town_point == tp) |>
+      select(town_point, xcoord, ycoord, transport_col) |>
+      na.omit()
+    
+    for(r in 1:(nrow(polyline_select)-1)) {
+      polyline_select_single_path <- polyline_select[c(r, r+1),]
+      map <- map |>
+        addPolylines(
+          group = paste0("PL", tp),
+          lng = polyline_select_single_path$xcoord,
+          lat = polyline_select_single_path$ycoord,
+          col = polyline_select_single_path$transport_col[2]
+        )
+    }
+    pb$tick()
+  }
+  saveRDS(map, file.path(fixtures_path, "base-map.rds"))
+  return(map)
+    
 }
