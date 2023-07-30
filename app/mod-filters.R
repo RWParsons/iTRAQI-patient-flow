399d0a1d0276c0d65d9055a90457dff29988aa54
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2023-07-30 18:20:20 +1000

    reorganise filtering code/reactives to reduce repetition

--- app/mod-filters.R
+++ app/mod-filters.R
@@ -117,32 +117,44 @@ server_map_filters <- function(id, passMap) {
       }
     })
 
-    observeEvent(list(
-      input$path_categories,
-      input$death_flags_cb,
-      input$travel_time_marker_col,
-      input$marker_groups,
-      input$age_category
-    ), ignoreNULL = FALSE, {
-      group_ids <- get_groups_path_cats(
+    group_ids <- reactive({
+      get_groups_path_cats(
         observed_paths = observed_paths,
         path_cats = input$path_categories,
         death_flag_select = input$death_flags_cb,
-        age_cats_select = input$age_category
+        age_cats_select = input$age_category,
+        travel_time_marker_col = input$travel_time_marker_col
       )
-
-      if (input$travel_time_marker_col) {
-        group_ids$show_groups <- paste0("traveltime-", group_ids$show_groups)
-        group_ids$hide_groups <- unique(c(paste0("traveltime-", group_ids$hide_groups), observed_paths$pu_id))
-      } else {
-        group_ids$hide_groups <- unique(c(group_ids$hide_groups, paste0("traveltime-", observed_paths$pu_id)))
-      }
-
-
+    })
+    
+    plot_df <- reactive({
+      observed_paths |> 
+        filter(pu_id %in% clean_show_pu_ids(group_ids()$show_groups)) |> 
+        select(pu_id, itraqi_pred, total_time) |> 
+        distinct()
+    })
+      
+    
+    output$plot <- renderPlot({
+      make_plot(plot_df(), input$plot_time_col)
+    })
+    
+    observeEvent(input$plot_brush, {
+      brushed_points <- brushedPoints(plot_df(), input$plot_brush)
+      print(brushed_points)
+    })
+    
+    output$freq_table <- renderTable({
+      make_freq_table(
+        show_ids = group_ids()$show_groups
+      )
+    })
+    
+    observeEvent(list(input$marker_groups, input$travel_time_marker_col), {
       if ("observed points" %in% input$marker_groups) {
         passMap() |>
-          hideGroup(group_ids$hide_groups) |>
-          showGroup(group_ids$show_groups)
+          hideGroup(group_ids()$hide_groups) |>
+          showGroup(group_ids()$show_groups)
       } else {
         passMap() |>
           hideGroup(c(paste0("traveltime-", observed_paths$pu_id), observed_paths$pu_id))
@@ -153,76 +165,6 @@ server_map_filters <- function(id, passMap) {
       } else {
         passMap() |> hideGroup(iTRAQI_paths$town_point)
       }
-
-      output$plot <- renderPlot({
-        plot_df <-
-          observed_paths |> 
-          filter(pu_id %in% paste0("ID-", unique(str_extract(group_ids$show_groups, "[0-9]*$")))) |> 
-          select(pu_id, itraqi_pred, total_time) |> 
-          distinct()
-        
-        make_plot(plot_df, input$plot_time_col)
-      })
-
-      observeEvent(input$plot_brush, {
-        plot_df <- observed_paths |>
-          filter(
-            !pu_id %in% group_ids$hide_groups,
-            pu_id %in% group_ids$show_groups
-          ) |>
-          # select(pu_id, itraqi_pred, total_time) |>
-          distinct()
-
-        brushed_points <- brushedPoints(plot_df, input$plot_brush)
-        print(brushed_points)
-        # browser()
-      })
-
-      output$freq_table <- renderTable({
-        age_cats_select <- input$age_category
-        age_cats_select[age_cats_select == ""] <- NA
-
-        # browser()
-        df_op <-
-          observed_paths |>
-          group_by(pu_id) |>
-          slice(1) |>
-          ungroup() |>
-          mutate(across(c(path_category, death_flag), as.factor))
-
-        df_op_freq <-
-          df_op |>
-          group_by(path_category, death_flag) |>
-          summarize(n_all = n())
-
-        df_op_freq <-
-          df_op |>
-          group_by(path_category, death_flag, .drop = FALSE) |>
-          summarize(n_all = n()) |>
-          pivot_wider(names_from = death_flag, values_from = n_all) |>
-          janitor::adorn_totals("row") |>
-          janitor::adorn_totals("col") |>
-          pivot_longer(!path_category, values_to = "all")
-
-        df_filtered_freq <-
-          df_op |>
-          filter(
-            path_category %in% input$path_categories,
-            death_flag %in% input$death_flags_cb,
-            cut(PAT_AGE, breaks = age_cat_breaks) %in% age_cats_select
-          ) |>
-          group_by(path_category, death_flag, .drop = FALSE) |>
-          summarize(n_filtered = n()) |>
-          pivot_wider(names_from = death_flag, values_from = n_filtered) |>
-          janitor::adorn_totals("row") |>
-          janitor::adorn_totals("col") |>
-          pivot_longer(!path_category, values_to = "filtered")
-
-        inner_join(df_op_freq, df_filtered_freq, by = c("path_category", "name")) |>
-          mutate(cell_content = paste0(filtered, "/", all)) |>
-          select(-all_of(c("all", "filtered"))) |>
-          pivot_wider(names_from = "name", values_from = "cell_content")
-      })
     })
   })
 }
@@ -254,3 +196,46 @@ make_plot <- function(plot_df, plot_time_col) {
       y = "Observed travel time (mins)"
     )
 }
+
+make_freq_table <- function(show_ids) {
+  df_op <-
+    observed_paths |>
+    group_by(pu_id) |>
+    slice(1) |>
+    ungroup() |>
+    mutate(across(c(path_category, death_flag), as.factor))
+  
+  df_op_freq <-
+    df_op |>
+    group_by(path_category, death_flag) |>
+    summarize(n_all = n())
+  
+  df_op_freq <-
+    df_op |>
+    group_by(path_category, death_flag, .drop = FALSE) |>
+    summarize(n_all = n()) |>
+    pivot_wider(names_from = death_flag, values_from = n_all) |>
+    janitor::adorn_totals("row") |>
+    janitor::adorn_totals("col") |>
+    pivot_longer(!path_category, values_to = "all")
+  
+  df_filtered_freq <-
+    df_op |>
+    filter(pu_id %in% clean_show_pu_ids(show_ids)) |>
+    group_by(path_category, death_flag, .drop = FALSE) |>
+    summarize(n_filtered = n()) |>
+    pivot_wider(names_from = death_flag, values_from = n_filtered) |>
+    janitor::adorn_totals("row") |>
+    janitor::adorn_totals("col") |>
+    pivot_longer(!path_category, values_to = "filtered")
+  
+  inner_join(df_op_freq, df_filtered_freq, by = c("path_category", "name")) |>
+    mutate(cell_content = paste0(filtered, "/", all)) |>
+    select(-all_of(c("all", "filtered"))) |>
+    pivot_wider(names_from = "name", values_from = "cell_content")
+}
+
+clean_show_pu_ids <- function(show_ids) {
+  paste0("ID-", unique(str_extract(show_ids, "[0-9]*$")))
+}
+
--- app/path-category-controls.R
+++ app/path-category-controls.R
@@ -7,7 +7,11 @@ age_cat_breaks <- c(0, 16, 24, 65, 99)
 age_cats <- c(levels(cut(observed_paths$PAT_AGE, breaks = age_cat_breaks)), NA)
 
 
-get_groups_path_cats <- function(observed_paths, path_cats, death_flag_select, age_cats_select) {
+get_groups_path_cats <- function(observed_paths,
+                                 path_cats, 
+                                 death_flag_select, 
+                                 age_cats_select, 
+                                 travel_time_marker_col) {
   age_cats_select[age_cats_select == ""] <- NA
 
   show_groups <- observed_paths |>
@@ -19,7 +23,14 @@ get_groups_path_cats <- function(observed_paths, path_cats, death_flag_select, a
     pull(pu_id)
 
   hide_groups <- observed_paths$pu_id[!observed_paths$pu_id %in% show_groups]
-
+  
+  if (travel_time_marker_col) {
+    show_groups <- paste0("traveltime-", show_groups)
+    hide_groups <- unique(c(paste0("traveltime-", hide_groups), observed_paths$pu_id))
+  } else {
+    hide_groups <- unique(c(hide_groups, paste0("traveltime-", observed_paths$pu_id)))
+  }
+  
   list(
     hide_groups = hide_groups,
     show_groups = show_groups
